33fd0cf23d7636ab14bdd23bf82aa02fbeb0e40b
rafacenter64 <108146187+rafacenter64@users.noreply.github.com>
2023-09-14 15:57:11 -0300

    linux version crieted

---
+++ linux.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+echo "connect scrcpy by wifi!"
+echo "connect the cable temporarily"
+sleep 3
+echo "Changing the WiFi server port"
+sleep 1
+adb tcpip 9839 
+echo "Connecting via WiFi"
+sleep 1
+echo "Enter Settings and About and copy the IP address"
+read IP
+adb connect $IP:9839
+echo "connecting in scrcpy"
+sleep 2
+scrcpy & disown
+exit
